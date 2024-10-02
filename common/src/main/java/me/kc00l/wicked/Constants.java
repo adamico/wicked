49985dbae2f90ceba2d73778bdb27e4c48a0b5cc
Andrea D'Amico <andrea.damico@gmail.com>
2024-10-02 18:47:38 +0200

    renamed/refactoring

---
+++ common/src/main/java/me/kc00l/wicked/Constants.java
@@ -0,0 +1,11 @@
+package me.kc00l.wicked;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "wicked";
+	public static final String MOD_NAME = "Wicked";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
